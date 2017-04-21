47a7a73489294c25a0f467e9747b08c7be73f795
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-04-21 14:31:53 +0900

    HP

---
+++ TD/Assets/Scripts/Base.cs
@@ -0,0 +1,50 @@
+﻿//================================
+//         Base Script
+//--------------------------------
+//@! Irfan Fahmi Ramadhan
+//@! 2017/04/21
+//================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+
+    private HP _hpBar;
+
+    public float hp;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //HP Barを取得する
+        _hpBar = transform.GetChild(0).GetChild(0).GetComponent<HP>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //HPを表示
+        _hpBar.currentHp = hp;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            //ダメージを受ける
+            TakeDamage(10);
+        }
+    }
+
+    //---------------------------
+    //@! 概要　：ダメージを受ける
+    //@! 引数　：ダメージ
+    //---------------------------
+    public void TakeDamage(float dmg)
+    {
+        hp -= dmg;
+    }
+}
---
+++ TD/Assets/Scripts/HP.cs
@@ -0,0 +1,47 @@
+﻿//================================
+//           HP Script
+//--------------------------------
+//@! Irfan Fahmi Ramadhan
+//@! 2017/04/21
+//================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+
+    //ターゲット
+    private Transform target;
+
+    //緑バー
+    private Image greenBar;
+
+    //現在HP
+    public float currentHp = 100;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentHp /= 100;
+        target = transform.parent.parent;
+        greenBar = transform.GetChild(1).GetComponent<Image>();
+    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //HPバーをターゲットの下に配置する
+        Vector3 wantedPos = Camera.main.WorldToScreenPoint(target.position);
+        transform.position = new Vector3(wantedPos.x, wantedPos.y - 20, wantedPos.z);
+
+        //バーの表示
+        greenBar.fillAmount = currentHp;
+
+
+		
+	}
+}
