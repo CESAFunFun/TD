be40af3553a5c1791369dfaeecc23263e0dd2878
TakayoshiOgawa <s152103>
2017-04-24 10:57:30 +0900

    マルチゲームパッドの更新とプレイヤースクリプトの修正

--- TD/Assets/Scripts/Player.cs
+++ TD/Assets/Scripts/Player.cs
@@ -9,74 +9,81 @@ public class Player : MonoBehaviour {
     public float rotaSpeed = 1F;
     public float shotPower = 8F;
 
-    private ObjectPool bulletPool;
-    private Rigidbody _rigidbody;
     private Vector3 _velocity;
     private Vector3 _direction;
+    private Rigidbody _rigidbody;
+    private ObjectPool _bulletPool;
+    private PlayerGamePad _gamepad;
 
 	// Use this for initialization
 	void Start () {
-        // 弾用の貯蔵庫を取得
-        bulletPool = GetComponent<ObjectPool>();
         // 物理演算を取得
         _rigidbody = GetComponent<Rigidbody>();
+        // 弾用の貯蔵庫を取得
+        _bulletPool = GetComponent<ObjectPool>();
+        // ゲームパッドの取得
+        _gamepad = GetComponent<PlayerGamePad>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        // フレーム毎に入力の初期化
+        // 移動量と向きを初期化
         _velocity = Vector3.zero;
         _direction = Vector3.zero;
 
-        // 横移動
-        if (Input.GetKey(KeyCode.A))
+        if (_gamepad.state != null)
         {
-            Move(Vector3.back, moveSpeed);
-            //_velocity = Vector3.back * moveSpeed;
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            Move(Vector3.forward, moveSpeed);
-            _velocity = Vector3.forward * moveSpeed;
-        }
+            // 移動処理
+            var moveAxis = _gamepad.state.LeftStickAxis;
+            Move(new Vector3(moveAxis.y, 0F, moveAxis.x), moveSpeed);
 
-        // 奥移動
-        if (Input.GetKey(KeyCode.W))
-        {
-            Move(Vector3.left, moveSpeed);
-            //_velocity = Vector3.left * moveSpeed;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            Move(Vector3.right, moveSpeed);
-            //_velocity = Vector3.right * moveSpeed;
-        }
+            // 回転処理
+            var rotaAxis = _gamepad.state.rightStickAxis;
+            Rotation(Vector3.up * rotaAxis.x, rotaSpeed);
 
-        // 回転処理
-        if (Input.GetKey(KeyCode.Q))
-        {
-            Rotation(Vector3.down, rotaSpeed);
-            //_direction = Vector3.up;
-        }
-        else if(Input.GetKey(KeyCode.E))
-        {
-            Rotation(Vector3.up, rotaSpeed);
-            //_direction = Vector3.down;
-        }
-
-        // プレハブの弾を発射
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            // オブジェクトプールで貯蔵している弾から発射
-            var bullet = bulletPool.GetGameObject();
-
-            // 非アクティブな弾があれば使用
-            if (bullet)
+            // プレハブの弾を発射
+            if (_gamepad.state.RightShoulder)
             {
-                bullet.transform.position = transform.position + transform.forward * 2F;
-                bullet.transform.rotation = Quaternion.identity;
-                bullet.GetComponent<Rigidbody>().velocity = transform.forward * shotPower;
+                Shot(shotPower);
+            }
+        }
+        else
+        {
+            // 横移動
+            if (Input.GetKey(KeyCode.A))
+            {
+                Move(Vector3.back, moveSpeed);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                Move(Vector3.forward, moveSpeed);
+            }
+
+            // 奥移動
+            if (Input.GetKey(KeyCode.W))
+            {
+                Move(Vector3.left, moveSpeed);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                Move(Vector3.right, moveSpeed);
+            }
+
+            // 回転処理
+            if (Input.GetKey(KeyCode.Q))
+            {
+                Rotation(Vector3.down, rotaSpeed);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                Rotation(Vector3.up, rotaSpeed);
+            }
+
+            // プレハブの弾を発射
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Shot(shotPower);
             }
         }
     }
@@ -94,5 +101,18 @@ public class Player : MonoBehaviour {
 
     public void Rotation(Vector3 direction, float speed) {
         _direction = direction * speed;
+    }
+
+    public void Shot(float power) {
+        // オブジェクトプールで貯蔵している弾から発射
+        var bullet = _bulletPool.GetGameObject();
+
+        // 非アクティブな弾があれば使用
+        if (bullet)
+        {
+            bullet.transform.position = transform.position + transform.forward * 2F;
+            bullet.transform.rotation = Quaternion.identity;
+            bullet.GetComponent<Rigidbody>().velocity = transform.forward * power;
+        }
     }
 }
