8c0a3544fdfe5c32b04d6c9de6f08e740d9438a1
Shodai Ando <guttan.tui@gmail.com>
2017-05-09 14:13:34 +0900

    プレイヤーの色変更可視化

---
+++ TD/Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerColor : MonoBehaviour {
+
+    //色
+    public Material[] _material;
+    public int ColorNumber;
+
+	// Use this for initialization
+	void Start () {
+        ColorNumber = 1;
+        gameObject.GetComponent<Renderer>().material = _material[ColorNumber];
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TD/Assets/Scripts/ShootColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootColor : MonoBehaviour {
+
+    [SerializeField]
+    PlayerColor colorNumber;
+
+	// Use this for initialization
+	void Start () {
+        Image image = GetComponent<Image>();
+        if (colorNumber.ColorNumber==1)
+        {
+            image.color = Color.green;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
